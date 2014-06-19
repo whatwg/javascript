090dc8ff1ef8d0d4fe20671298cadc083ffcd1bb
Mathias Bynens <mathias@qiwi.be>
2014-06-19 15:26:29 +0200

    Clarify goals

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Allow running this script from another directory
-cd "$(dirname "${BASH_SOURCE}")"
+cd "$(dirname "${BASH_SOURCE}")";
 
 # Generate the spec
-anolis --output-encoding=utf-8 --omit-optional-tags --quote-attr-values --enable=xspecxref --enable=refs --w3c-shortname="javascript" index.src.html index.html
+anolis --output-encoding=utf-8 --omit-optional-tags --quote-attr-values --enable=xspecxref --enable=refs --w3c-shortname="javascript" index.src.html index.html;
